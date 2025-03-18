640eaac4e80393de5b31f7f2defda085c50d6f1a
marouane <marouanelakrad17@gmail.com>
2025-03-17 22:17:07 -0400

    add some modification

--- exercice4.sh
+++ exercice4.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Please enter a string :"
 read user_input
+reversed=""
 for (( i = ${#user_input}-1 ; i >= 0 ; i--)); do
-	user_input="${user_input:${i}:1}"
+	reversed="${reversed}${user_input:${i}:1}"
 done	
+ echo "read the reverse string : ${reversed}"
