719d29b0001cf62ad0f01f631dffc73da65aa2dc
marouane <marouanelakrad17@gmail.com>
2025-03-23 23:47:23 -0400

    edit the exercice14

--- exercice14.sh
+++ exercice14.sh
@@ -1,6 +1,26 @@
 #!/bin/bash 
-for file in *; do
-  echo "$file"
+#for file in *; do
+#  echo "$file"
+#done
+
+
+#files=($(ls))
+
+#i=0
+
+#while [ $i -lt ${#files[@]} ]; do
+#  echo "${files[$i]}" 
+#  ((i++))  
+#done
+	
+
+#!/bin/bash
+
+
+files=($(ls))
+
+
+for ((index=0; index<${#files[@]}; index++)); do
+  echo "${files[$index]}"  
 done
 
-other ways whil and for with condition
